23f9fc85812be40ccf141ae68ae1cb5b7ba7c2dd
dr8297 <david.rodriguezmartin@gmail.com>
2018-08-27 17:56:57 +0100

    First commit

---
+++ Debug/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_core.d
@@ -0,0 +1,125 @@
+Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_core.o: \
+ ../Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_core.c \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Inc/usbd_conf.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xc.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/CMSIS/Include/core_cmSimd.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Inc/stm32f4xx_hal_conf.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Inc/main.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h \
+ C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Inc/usbd_conf.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xc.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/CMSIS/Include/core_cm4.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/CMSIS/Include/core_cmSimd.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Inc/stm32f4xx_hal_conf.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Inc/main.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h:
+
+C:/Users/David/Documents/STM32/STM32F4_Disco/disco_project/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h:
